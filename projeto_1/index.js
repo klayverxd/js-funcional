536675e4894d23f23933c367cc0893955855286c
Klayver Ximenes <klayverx@gmail.com>
2021-05-13 09:47:10 -0300

    lendo conteúdo dos arquivos

--- projeto_1/index.js
+++ projeto_1/index.js
@@ -5,4 +5,5 @@ const caminho = path.join(__dirname, '..', 'dados', 'legendas')
 
 fn.lerDiretorio(caminho)
 	.then(arquivos => fn.elementosTerminadosCom(arquivos, '.srt'))
+	.then(arquivosSRT => fn.lerArquivos(arquivosSRT))
 	.then(console.log)
